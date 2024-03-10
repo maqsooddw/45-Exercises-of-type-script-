8575667b3867fcc80cd76ecd236b28b191874e39
maqsooddw <155903860+maqsooddw@users.noreply.github.com>
2024-03-10 22:00:18 +0500

    first commit

---
+++ 05_Famous Quote2/app.ts
@@ -0,0 +1,22 @@
+let famousPerson = "Ali Ibn Abi Talib (R.A)"
+let message = "once said,\“The richest of the rich is the one who is not a prisoner to greed.\”"
+
+console.log("•••••••••••••••••••••••••••••••••••••••••••••••••Exercise 05•••••••••••••••••••••••••••••••••••••••••••••••••••••");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•    ",famousPerson,message,"   •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•                                                                                                               •");
+console.log("•••••••••••••••••••••••••••••••••This program is created by Maqsood Ahmed••••••••••••••••••••••••••••••••••••••••");
